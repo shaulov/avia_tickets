983be527fe1b211e480ed2735ec6c31445cf5a6f
Skinny <ivan.shaulov@mail.ru>
2020-06-11 18:08:00 +0300

    added format for date

--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -27,6 +27,7 @@ export function getAutocompleteInstance(elem) {
 const datepickers = document.querySelectorAll('.datepicker');
 M.Datepicker.init(datepickers, {
     showClearBtn: true,
+    format: 'yyyy-mm',
 });
 
 export function getDatepickerInstance(elem) {

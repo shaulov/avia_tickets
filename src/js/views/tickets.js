19a951f18eaa3da68b142d4500cc474d0bbcc787
Skinny <ivan.shaulov@mail.ru>
2020-06-14 21:37:45 +0300

    added favorite button in ticket template

--- src/js/views/tickets.js
+++ src/js/views/tickets.js
@@ -68,6 +68,8 @@ class TicketsUI {
                     <span class="ticket-transfers">Пересадок: ${ticket.transfers}</span>
                     <span class="ticket-flight-number">Номер рейса: ${ticket.flight_number}</span>
                 </div>
+                <a class="waves-effect waves-light btn-small green darken-1 add-favorite ml-auto">Add to
+                            favorites</a>
             </div>
         </div>
         `;

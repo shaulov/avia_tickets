7973cbdb904e6100d11812e601d7a5a258aabef1
Skinny <ivan.shaulov@mail.ru>
2020-08-28 00:38:20 +0300

    realized adding to favorites

--- src/js/app.js
+++ src/js/app.js
@@ -4,16 +4,22 @@ import locations from './store/location';
 import formUI from './views/form';
 import ticketsUI from './views/tickets';
 import currencyUI from './views/currency';
+import favoriteTickets from './store/favoriteTickets';
 
 
 document.addEventListener('DOMContentLoaded', () => {
     initApp();
     const form = formUI.form;
+    const ticketSection = document.querySelector('.tickets-sections');
+    // const favoriteBtns = document.querySelectorAll('.add-favorite');
 
     // Events
     form.addEventListener('submit', e => {
         e.preventDefault();
         onFormSubmit();
+    });
+    ticketSection.addEventListener('click', e => {
+        onFavoriteClick(e);
     });
 
     // Handlers
@@ -38,5 +44,14 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
         ticketsUI.renderTickets(locations.lastSearch);
+    }
+
+    function onFavoriteClick(event) {
+        const target = event.target;
+        const btnAddToFavorite = target.closest('.add-favorite');
+        if (btnAddToFavorite) {
+            const ticket = btnAddToFavorite.closest('.ticket-card');
+            favoriteTickets.addToFavorite(ticket);
+        }
     }
 });
--- src/js/store/favoriteTickets.js
+++ src/js/store/favoriteTickets.js
@@ -1,9 +1,57 @@
+import ticketsUI from '../views/tickets';
+
 class FavoriteTickets {
     constructor() {
+        this.container = document.querySelector('.dropdown-content');
+    }
 
+    addToFavorite(ticket) {
+        const airlineImg = ticket.querySelector('.ticket-airline-img').src;
+        const origin = ticket.querySelectorAll('.ticket-city')[0].textContent;
+        const destination = ticket.querySelectorAll('.ticket-city')[1].textContent;
+        const date = ticket.querySelector('.ticket-time-departure').textContent;
+        const price = ticket.querySelector('.ticket-price').textContent;
+        const transfers = ticket.querySelector('.ticket-transfers').textContent;
+        const flightNumber = ticket.querySelector('.ticket-flight-number').textContent;
+        console.log(origin, destination, date, price, transfers, flightNumber);
+        const template = FavoriteTickets.favoriteTicketTemplate(airlineImg, origin, destination, date, price, transfers, flightNumber);
+        this.container.insertAdjacentHTML('beforeend', template);
+    }
+
+    deleteFromFavorites(ticket) {
+        this.container.remove(ticket);
+        // реализовать удаление билета
+    }
+
+    static favoriteTicketTemplate(airlineImg, origin, destination, date, price, transfers, flightNumber) {
+        return `<div class="favorite-item  d-flex align-items-start">
+                    <img src="${airlineImg}" class="favorite-item-airline-img" />
+                    <div class="favorite-item-info d-flex flex-column">
+                        <div class="favorite-item-destination d-flex align-items-center">
+                            <div class="d-flex align-items-center mr-auto">
+                                <span class="favorite-item-city">${origin}</span>
+                                <i class="medium material-icons">flight_takeoff</i>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <i class="medium material-icons">flight_land</i>
+                                <span class="favorite-item-city">${destination}</span>
+                            </div>
+                        </div>
+                        <div class="ticket-time-price d-flex align-items-center">
+                            <span class="ticket-time-departure">${date}</span>
+                            <span class="ticket-price ml-auto">${price}</span>
+                        </div>
+                        <div class="ticket-additional-info">
+                            <span class="ticket-transfers">${transfers}</span>
+                            <span class="ticket-flight-number">${flightNumber}</span>
+                        </div>
+                        <a
+                            class="waves-effect waves-light btn-small pink darken-3 delete-favorite ml-auto">Delete</a>
+                    </div>
+                </div>`;
     }
 }
 
-const favoriteTickets = new FavoriteTickets();
+const favoriteTickets = new FavoriteTickets(ticketsUI);
 
 export default favoriteTickets;
